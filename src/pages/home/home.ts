a8d4b3151cbbbef6467133ed25158041d7788fcd
Wesley Monaro <wesleymonaro@hotmail.com>
2017-11-02 23:27:19 -0200

    loading

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, LoadingController, Loading } from 'ionic-angular';
 
 import { Task } from "../../models/task.model";
 import { TaskService } from "../../providers/task/task.service";
@@ -13,15 +13,25 @@ export class HomePage {
   tasks: Task[] = [];
 
   constructor(
+    public loadingCtrl: LoadingController,
     public navCtrl: NavController,
     public taskService: TaskService
   ) { }
 
-  ionViewDidLoad(){
+  ionViewDidLoad() {
     this.taskService.getAll()
       .then((tasks: Task[]) => {
         this.tasks = tasks;
       })
+  }
+
+  private showLoading(message?: string): Loading {
+    let loading: Loading = this.loadingCtrl.create({
+      content: message || "Please wait..."
+    });
+    loading.present();
+    return loading;
+
   }
 
 }
