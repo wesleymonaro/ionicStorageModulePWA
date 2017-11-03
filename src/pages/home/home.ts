2400052a909861873746dcddf0ff615f28ffaccc
Wesley Monaro <wesleymonaro@hotmail.com>
2017-11-02 23:38:41 -0200

    save

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, LoadingController, Loading } from 'ionic-angular';
+import { NavController, LoadingController, Loading, ItemSliding, AlertOptions, AlertController } from 'ionic-angular';
 
 import { Task } from "../../models/task.model";
 import { TaskService } from "../../providers/task/task.service";
@@ -13,6 +13,7 @@ export class HomePage {
   tasks: Task[] = [];
 
   constructor(
+    public alertCtrl: AlertController,
     public loadingCtrl: LoadingController,
     public navCtrl: NavController,
     public taskService: TaskService
@@ -25,13 +26,60 @@ export class HomePage {
       })
   }
 
+  private showAlert(options: { itemSliding: ItemSliding, title: string, type: string }, task?: Task): void {
+    let alertOptions: AlertOptions = {
+      title: options.title,
+      inputs: [
+        {
+          name: 'title',
+          placeholder: 'Task Title'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel'
+        },
+        {
+          text: 'Save',
+          handler: (data) => {
+            let loading: Loading = this.showLoading(`Saving ${data.title} task...`);
+            let contextTask: Task;
+
+            switch (options.type) {
+              case 'create':
+                contextTask = new Task(data.title);
+                break;
+
+              case 'update':
+                task.title = data.title;
+                contextTask = task
+                break;
+
+            }
+            this.taskService[options.type](contextTask)
+              .then((savedTask: Task) => {
+                if (options.type == 'create') this.tasks.unshift(savedTask);
+                loading.dismiss();
+                if (options.itemSliding) options.itemSliding.close();
+              })
+          }
+        }
+      ]
+    }
+    if(options.type === 'update'){
+      alertOptions.inputs[0].value = task.title;
+    }
+
+    this.alertCtrl.create(alertOptions).present();
+  }
+
   private showLoading(message?: string): Loading {
     let loading: Loading = this.loadingCtrl.create({
       content: message || "Please wait..."
     });
     loading.present();
     return loading;
-
   }
 
 }
