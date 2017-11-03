e4d0905df750766a07932f39d7651ff74f0b3c9b
Wesley Monaro <wesleymonaro@hotmail.com>
2017-11-02 23:02:01 -0200

    getAll

--- src/providers/task/task.service.ts
+++ src/providers/task/task.service.ts
@@ -1,13 +1,27 @@
 import { Injectable } from '@angular/core';
 import { Storage } from '@ionic/storage';
 
+import { Task } from './../../models/task.model';
+
 
 @Injectable()
 export class TaskService {
 
   constructor(
     public storage: Storage
-  ) {
-  }
+  ) { }
 
+  getAll(): Promise<Task[]> {
+
+    return this.storage.ready()
+      .then((localForage: LocalForage) => {
+        let tasks: Task[] = [];
+
+        return this.storage.forEach((task: Task, key: string, iterationNumber: number) => {
+          if (key.indexOf('tasks.') > -1) {
+            tasks.push(task);
+          }
+        }).then(() => tasks);
+      }).catch(err => console.log(err));
+  }
 }
