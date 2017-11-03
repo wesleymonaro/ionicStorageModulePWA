a42a26b428defe8d445c3e05174f7eb0a7f752a1
Wesley Monaro <wesleymonaro@hotmail.com>
2017-11-03 00:31:42 -0200

    getAllFromStorage

--- src/providers/offline/offline.service.ts
+++ src/providers/offline/offline.service.ts
@@ -7,12 +7,27 @@ import { BaseModel } from "../../interfaces/base-model.interface";
 export abstract class OfflineService<T extends BaseModel>{
 
 
-    constructor(
-        private http: Http,
-        private itemApiUrl: string,
-        private network: Network,
-        private resourceName: string,
-        private storage: Storage
-    ){ }
+  constructor(
+    private http: Http,
+    private itemApiUrl: string,
+    private network: Network,
+    private resourceName: string,
+    private storage: Storage
+  ) { }
+
+  private getAllFromStorage(): Promise<T[]> {
+    return this.storage.ready()
+      .then((localForage: LocalForage) => {
+        let items: T[] = [];
+
+        return this.storage.forEach((value: any, key: string, iterationNumber: number) => {
+          if (key.indexOf(`${this.resourceName}.`) > -1) {
+            items.push(value);
+          }
+        }).then(() => {
+          return items;
+        })
+      })
+  }
 
 }
