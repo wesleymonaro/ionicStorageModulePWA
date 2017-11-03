3c0888105f48b0d50c2d670576b276cd13af8b26
Wesley Monaro <wesleymonaro@hotmail.com>
2017-11-02 22:53:39 -0200

    Criando TaskModel

---
+++ src/models/task.model.ts
@@ -0,0 +1,12 @@
+export class Task{
+
+    public id: number;
+    public done: boolean;
+
+    constructor(
+        public title: string
+    ){
+        this.id = new Date().getTime();
+        this.done = false;
+    }
+}
