3554a1c337ac112b4656f5ad4e03404cb071a969
Wesley Monaro <wesleymonaro@hotmail.com>
2017-11-02 23:23:42 -0200

    Interface HomePage

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { Task } from "../../models/task.model";
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+
+  tasks: Task[] = [
+    new Task("Estudar Angular"),
+    new Task("Estudar Ionic")
+  ];
 
   constructor(public navCtrl: NavController) {
 
