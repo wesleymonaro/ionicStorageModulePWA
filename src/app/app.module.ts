72eae18beae436b6d61ecb309dd949145b927512
Wesley Monaro <wesleymonaro@hotmail.com>
2017-11-02 22:51:58 -0200

    Configure StorageModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicStorageModule } from '@ionic/storage';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
@@ -14,7 +15,12 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    IonicStorageModule.forRoot({
+      name: '__tasksDB',
+      storeName: 'tasks',
+      driverOrder: ['sqlite', 'indexddb', 'websql', 'localstorage']
+    })
   ],
   bootstrap: [IonicApp],
   entryComponents: [
